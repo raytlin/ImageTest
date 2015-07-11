3932710b54a5c281316d41d4f43e24d1b772ea84
Ray Lin <raytlin@gmail.com>
2015-07-10 19:40:02 -0700

    focus point working

--- ImageTest/CameraViewController.m
+++ ImageTest/CameraViewController.m
@@ -13,24 +13,63 @@
 @property (weak, nonatomic) IBOutlet UILabel *lensLabel;
 @property (weak, nonatomic) IBOutlet UIView *captureView;
 @property (nonatomic) AVCaptureDevice *videoCaptureDevice;
-
+@property (nonatomic) AVCaptureVideoPreviewLayer *previewLayer;
+@property (nonatomic) UIView *focusSquare;
 @end
 
 @implementation CameraViewController
 
+-(void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Hide navigation controller
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    
+    // Add gesture recognizer to toggle navigation bar
+    UISwipeGestureRecognizer *swipeGestureDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(navBarDown)];
+    swipeGestureDown.direction = UISwipeGestureRecognizerDirectionDown;
+    UISwipeGestureRecognizer *swipeGestureUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(navBarUp)];
+    swipeGestureUp.direction = UISwipeGestureRecognizerDirectionUp;
+    
+    [self.view addGestureRecognizer:swipeGestureDown];
+    [self.view addGestureRecognizer:swipeGestureUp];
+    
+    // Create focus square and set to center of screen
+    self.focusSquare = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 30, 30)];
+    self.focusSquare.center = self.view.center;
+    self.focusSquare.layer.borderColor = [UIColor yellowColor].CGColor;
+    self.focusSquare.layer.borderWidth = 1.0f;
+    [self.view addSubview:self.focusSquare];
+}
+
+-(void)navBarDown {
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+}
+
+-(void)navBarUp {
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+// Get rid of status bar
+-(BOOL)prefersStatusBarHidden{
+    return YES;
+}
+
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
+    // Set up the camera and preview layer
     AVCaptureSession *captureSession = [[AVCaptureSession alloc] init];
     self.videoCaptureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     NSError *error = nil;
     AVCaptureDeviceInput *videoInput = [AVCaptureDeviceInput deviceInputWithDevice:self.videoCaptureDevice error:&error];
     if (videoInput) {
         [captureSession addInput:videoInput];
-        AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:captureSession];
+        self.previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:captureSession];
         UIView *aView = self.captureView;
-        previewLayer.frame = aView.bounds; // Assume you want the preview layer to fill the view.
-        [aView.layer addSublayer:previewLayer];
-        previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+        self.previewLayer.frame = [[UIScreen mainScreen] bounds];
+        [aView.layer addSublayer:self.previewLayer];
+        self.previewLayer.videoGravity = AVLayerVideoGravityResizeAspect;
         [captureSession startRunning];
         [self.videoCaptureDevice addObserver:self forKeyPath:@"lensPosition" options:NSKeyValueObservingOptionNew context:nil];
     }
@@ -54,21 +93,21 @@
 
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     [touches enumerateObjectsUsingBlock:^(id obj, BOOL *stop) {
+        
         UITouch *touch = obj;
         CGPoint touchPoint = [touch locationInView:touch.view];
-        CGRect screenRect = [[UIScreen mainScreen] bounds];
-        CGFloat screenWidth = screenRect.size.width;
-        CGFloat screenHeight = screenRect.size.height;
-        double focus_x = touchPoint.x/screenWidth;
-        double focus_y = touchPoint.y/screenHeight;
+        CGPoint focusPoint = [self.previewLayer captureDevicePointOfInterestForPoint:touchPoint];
         
         NSError *error =nil;
         [self.videoCaptureDevice lockForConfiguration:&error];
+        self.videoCaptureDevice.focusPointOfInterest = focusPoint;
         self.videoCaptureDevice.focusMode = AVCaptureFocusModeAutoFocus;
-        self.videoCaptureDevice.focusPointOfInterest = CGPointMake(focus_x,focus_y);
         if (error) {
             NSLog(@"%@", error);
         }
+        // Set the focus indicator 
+        self.focusSquare.center = touchPoint;
+        [self.videoCaptureDevice unlockForConfiguration];
     }];
 }
 
